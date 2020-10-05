7dee435966cdf91d3e22b4ef4676eaadad84e2ee
Simon <sacost6@uic.edu>
2020-10-04 20:09:08 -0500

    initial commit

---
+++ app/src/main/java/com/example/project2/ImageModel.java
@@ -0,0 +1,11 @@
+package com.example.project2;
+
+public class ImageModel {
+    int mThumbIds;
+    public int getmThumbIds() {
+        return mThumbIds;
+    }
+    public void setmThumbIds(int mThumbIds) {
+        this.mThumbIds = mThumbIds;
+    }
+}
---
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.project2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    int image[] = {R.drawable.nissanskyline, R.drawable.hellcat, R.drawable.foxbodymustang,
+            R.drawable.c4corvette, R.drawable.rollsroyceseries2, R.drawable.mclaren600lt,
+            R.drawable.hvipergt, R.drawable.chargergt, R.drawable.fourrunner};
+
+    GridView gridView;
+    ArrayList<ImageModel> arrayList;
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.gridview);
+        arrayList = new ArrayList<>();
+        for (int i = 0; i < image.length; i++) {
+            ImageModel imagemodel = new ImageModel();
+            imagemodel.setmThumbIds(image[i]);
+            //add in array list
+            arrayList.add(imagemodel);
+        }
+
+        ImageAdapter adpter= new ImageAdapter(getApplicationContext(), arrayList);
+        gridView.setAdapter(adpter);
+        //item click listner
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+            }
+        });
+    }
+}
